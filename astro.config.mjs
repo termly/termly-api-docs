1ec5cbf024fc2ca2975e83ab070d7a4d47043c81
Zach Alvstad <99202075+zalvstad-termly@users.noreply.github.com>
2025-04-01 13:41:08 -0500

    reorganize sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,24 +22,49 @@ export default defineConfig({
 				{
 					label: 'Endpoints',
 					items: [
-						{ label: '(GET) Banners', slug: 'endpoints/banners-get' },
-						{ label: '(PUT) Banners', slug: 'endpoints/banners-put' },
-						{ label: '(GET) Collaborators', slug: 'endpoints/collaborators-get' },
-						{ label: '(PUT) Collaborators', slug: 'endpoints/collaborators-put' },
-						{ label: '(POST) Collaborators', slug: 'endpoints/collaborators-post' },
-						{ label: '(DELETE) Collaborators', slug: 'endpoints/collaborators-delete' },
-						{ label: '(GET) Cookies', slug: 'endpoints/cookies-get' },
-						{ label: '(PUT) Cookies', slug: 'endpoints/cookies-put' },
-						{ label: '(POST) Cookies', slug: 'endpoints/cookies-post' },
-						{ label: '(DELETE) Cookies', slug: 'endpoints/cookies-delete' },
-						{ label: '(GET) Custom Consent Themes', slug: 'endpoints/custom-consent-themes-get' },
-						{ label: '(PUT) Custom Consent Themes', slug: 'endpoints/custom-consent-themes-put' },
-						{ label: '(POST) Custom Consent Themes', slug: 'endpoints/custom-consent-themes-post' },
-						{ label: '(DELETE) Custom Consent Themes', slug: 'endpoints/custom-consent-themes-delete' },
-						{ label: '(GET) Websites', slug: 'endpoints/websites-get' },
-						{ label: '(PUT) Websites', slug: 'endpoints/websites-put' },
-						{ label: '(POST) Websites', slug: 'endpoints/websites-post' },
-						{ label: '(DELETE) Websites', slug: 'endpoints/websites-delete' },
+						{
+							label: 'Banners',
+							items: [
+								{ label: 'GET', slug: 'endpoints/banners-get' },
+								{ label: 'PUT', slug: 'endpoints/banners-put' },
+							]
+						},
+						{
+							label: 'Collaborators',
+							items: [
+								{ label: 'GET', slug: 'endpoints/collaborators-get' },
+								{ label: 'PUT', slug: 'endpoints/collaborators-put' },
+								{ label: 'POST', slug: 'endpoints/collaborators-post' },
+								{ label: 'DELETE', slug: 'endpoints/collaborators-delete' },
+							]
+						},
+						{
+							label: 'Cookies',
+							items: [
+								{ label: 'GET', slug: 'endpoints/cookies-get' },
+								{ label: 'PUT', slug: 'endpoints/cookies-put' },
+								{ label: 'POST', slug: 'endpoints/cookies-post' },
+								{ label: 'DELETE', slug: 'endpoints/cookies-delete' },
+							]
+						},
+						{
+							label: 'Custom Consent Themes',
+							items: [
+								{ label: 'GET', slug: 'endpoints/custom-consent-themes-get' },
+								{ label: 'PUT', slug: 'endpoints/custom-consent-themes-put' },
+								{ label: 'POST', slug: 'endpoints/custom-consent-themes-post' },
+								{ label: 'DELETE', slug: 'endpoints/custom-consent-themes-delete' },
+							]
+						},
+						{
+							label: 'Websites',
+							items: [
+								{ label: 'GET', slug: 'endpoints/websites-get' },
+								{ label: 'PUT', slug: 'endpoints/websites-put' },
+								{ label: 'POST', slug: 'endpoints/websites-post' },
+								{ label: 'DELETE', slug: 'endpoints/websites-delete' },
+							]
+						},
 					],
 				},
 			],
